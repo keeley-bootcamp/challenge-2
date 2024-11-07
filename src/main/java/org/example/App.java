99b96be4696ba9b2b4610f20ed3a933385e8286d
keeley-bootcamp <116581328+keeley1@users.noreply.github.com>
2024-11-07 11:21:21 +0000

    Adding files

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class App {
+    public static void main( String[] args ) {
+       addInts(100, 200);
+    }
+
+    public static void addInts(int a, int b) {
+        int result = a + b;
+        System.out.println("The result of " + a + " + " + b + " is " + result);
+    }
+}
